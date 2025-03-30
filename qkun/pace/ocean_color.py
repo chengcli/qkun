27a32a4488d8451895be2c5a3b7ab1cdcc0c410d
Cheng Li <69489965+chengcli@users.noreply.github.com>
2025-03-30 15:32:48 -0400

    Finish pipeline for PACE (#4)

--- qkun/pace/ocean_color.py
+++ qkun/pace/ocean_color.py
@@ -1,12 +1,23 @@
 import yaml
 import numpy as np
 import os
-from typing import Tuple
+import asyncio
+from typing import Tuple, Optional
 from netCDF4 import Dataset
+from ..geobox import GeoBox
+from ..cmr.granule_download import GranuleDownloader
 from ..granule.append_resolution import append_resolution_to_yaml
 from ..granule.compute_alpha_envelope import compute_alpha_envelope
 from ..granule.granule_handler import GranuleHandler
-from ..geobox import GeoBox
+
+def read_var_masked(group, var_name, dtype=np.float32):
+    var = group.variables[var_name]
+    data = var[:].astype(dtype)
+    fill_value = var.getncattr("_FillValue") if "_FillValue" in var.ncattrs() else None
+    data = np.ma.masked_invalid(data)
+    if fill_value is not None:
+        data = np.ma.masked_equal(data, fill_value)
+    return data
 
 def process_footprint(nc_path: str, yaml_file: str=None, verbose: bool=True) -> Tuple[np.ndarray, np.ndarray]:
     """Extracts latitude and longitude from 'geolocation_data' group and caches to .npz file."""
@@ -20,18 +31,8 @@ def process_footprint(nc_path: str, yaml_file: str=None, verbose: bool=True) -> 
         raise ValueError("Group 'geolocation_data' not found in NetCDF file.")
 
     geo_group = ds.groups["geolocation_data"]
-
-    def read_var_masked(var_name):
-        var = geo_group.variables[var_name]
-        data = var[:].astype(np.float32)
-        fill_value = var.getncattr("_FillValue") if "_FillValue" in var.ncattrs() else None
-        data = np.ma.masked_invalid(data)
-        if fill_value is not None:
-            data = np.ma.masked_equal(data, fill_value)
-        return data
-
-    lat = read_var_masked("latitude")
-    lon = read_var_masked("longitude")
+    lat = read_var_masked(geo_group, "latitude")
+    lon = read_var_masked(geo_group, "longitude")
 
     if yaml_file is not None:
         append_resolution_to_yaml(lat, lon, yaml_file)
@@ -52,29 +53,40 @@ class OceanColor(GranuleHandler):
         self.prefix = path
         self.basename = os.path.splitext(os.path.splitext(basename)[0])[0]
 
+    def __del__(self):
+        if hasattr(self, "ds"):
+            self.ds.close()
+
     def process(self, alpha: float=0.0, max_points: int=1000):
         digest_path = f"{os.path.join(self.prefix, self.basename)}.global.yaml"
         with open(digest_path, "r") as f:
             digest = yaml.safe_load(f)
 
+        # footprint
         if not os.path.exists(self.footprint_path()):
-            footprint_path = process_footprint(
-                    digest["data_path"], digest_path, self.verbose)
+            nc_file = digest["data_path"]
+            if not os.path.exists(nc_file):
+                # download data
+                downloader = GranuleDownloader(self.USERNAME, self.PASSWORD,
+                        self.prefix, verbose=self.verbose)
+                url = digest["url_path"]
+                asyncio.run(downloader.download(url))
+            footprint_path = process_footprint(nc_file, digest_path, self.verbose)
         else:
             footprint_path = self.footprint_path()
 
-        if not os.path.exists(self.fov_path(alpha)): 
-            fov_path = compute_alpha_envelope(
-                    footprint_path, alpha, max_points, self.verbose)
-        else:
-            fov_path = self.fov_path(alpha)
+        # field of view
+        if not os.path.exists(self.fov_path(alpha)):
+            compute_alpha_envelope(footprint_path,
+                                   alpha, max_points, self.verbose)
+
 
         if self.verbose:
             print(f"Processing done: {self.basename}")
 
     def get_fov(self, alpha: float=0.0) -> Tuple[np.ndarray, np.ndarray]:
         if not os.path.exists(self.fov_path(alpha)):
-            raise ValueError(f"File not found: {self.fov_path(alpha)}")
+            raise ValueError(f"FOV file not found: {self.fov_path(alpha)}")
 
         fov_path = self.fov_path(alpha)
         data = np.genfromtxt(fov_path)
@@ -82,7 +94,7 @@ class OceanColor(GranuleHandler):
 
     def get_bounding_box(self) -> GeoBox:
         if not os.path.exists(self.digest_path()):
-            raise ValueError(f"File not found: {self.digest_path()}")
+            raise ValueError(f"Digest file not found: {self.digest_path()}")
 
         digest_path = self.digest_path()
         with open(digest_path, "r") as f:
@@ -96,18 +108,38 @@ class OceanColor(GranuleHandler):
 
     def get_footprint(self) -> Tuple[np.ndarray, np.ndarray]:
         if not os.path.exists(self.footprint_path()):
-            raise ValueError(f"File not found: {self.footprint_path()}")
+            raise ValueError(f"Footprint file not found: {self.footprint_path()}")
 
-        footprint_path = self.footprint_path()
-        data = np.load(footprint_path)
+        data = np.load(self.footprint_path())
         return data["longitude"], data["latitude"]
 
-    def get_data(self) -> dict:
-        """Extracts data from 'geophysical_data' group and caches to .npz file."""
-        basename = os.path.basename(nc_file)
-        output_file = f'{os.path.splitext(basename)[0]}.data.npz'
+    def get_data(self, name: Optional[str]=None) -> dict:
+        if not os.path.exists(self.digest_path()):
+            raise ValueError(f"Digest file not found: {self.digest_path()}")
 
-        ds = Dataset(nc_file, mode="r")
+        digest_path = self.digest_path()
+        with open(digest_path, "r") as f:
+            digest = yaml.safe_load(f)
 
-        print(f"Data saved to: {output_file}")
-        return {"chlor_a": chlor_a, "sst": sst}
+        nc_file = digest["data_path"]
+        if not os.path.exists(nc_file):
+            if self.verbose:
+                print(f"Downloading data from: {digest['url_path']}")
+            # download data
+            downloader = GranuleDownloader(self.USERNAME, self.PASSWORD,
+                    self.prefix, verbose=self.verbose)
+            url = digest["url_path"]
+            asyncio.run(downloader.download(url))
+
+        self.ds = Dataset(nc_file, mode="r")
+
+        if "observation_data" not in self.ds.groups:
+            raise ValueError("Group 'observation_data' not found in NetCDF file.")
+        obs_group = self.ds.groups["observation_data"]
+
+        if name is None:
+            return obs_group
+        else:
+            if name not in obs_group.variables:
+                raise ValueError(f"Variable '{name}' not found in Group 'observation_data'.")
+            return obs_group.variables[name]
